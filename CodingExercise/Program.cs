9bb9f6640a5f878285ed6c6bdd1f08cfecb1f30d
willis-aj <willis.aj@outlook.com>
2024-05-07 17:50:00 -0500

    wrote withdrawl method and cleaned up som of the deposit code

--- CodingExercise/Program.cs
+++ CodingExercise/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,18 +12,17 @@ var app = builder.Build();
 var myApi = app.MapGroup("/deposit");
 myApi.MapPost("/", (HttpRequest request) =>
 {
-    var jsonDepositRequest = request.ReadFromJsonAsync<Bank.DepositRequest>();
-    Bank.DepositRequest depositRequest = new Bank.DepositRequest(jsonDepositRequest.Result.CustomerId, jsonDepositRequest.Result.AccountId, jsonDepositRequest.Result.Amount);
-    Bank bank = new Bank();
-    Bank.DepositResponse response = bank.MakeDeposit(depositRequest);
-    return Results.Json(response, typeof(Bank.DepositResponse), SourceGenerationContext.Default);
+    var jsonDepositRequest = request.ReadFromJsonAsync<BalanceChangeRequest>();
+    BalanceChangeRequest depositRequest = new BalanceChangeRequest(jsonDepositRequest.Result.CustomerId, jsonDepositRequest.Result.AccountId, jsonDepositRequest.Result.Amount);
+
+    return Results.Json(Bank.MakeDeposit(depositRequest), typeof(BalanceChangeResponse), SourceGenerationContext.Default);
 });
 
 app.Run();
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
-[JsonSerializable(typeof(Bank.DepositResponse))]
-[JsonSerializable(typeof(Bank.DepositRequest))]
+[JsonSerializable(typeof(BalanceChangeResponse))]
+[JsonSerializable(typeof(BalanceChangeRequest))]
 internal partial class SourceGenerationContext : JsonSerializerContext
 {
 }
@@ -34,71 +30,103 @@ internal partial class SourceGenerationContext : JsonSerializerContext
 
 class Bank
 {
-    Account[] sampleAccounts =
+    //would have liked to make a factory to generate different kinds of accounts (checking, savings, etc.)
+    //this is pretend db
+    static Account[] sampleAccounts =
         {
             new(1, 1, 100),
             new(2, 2, 200),
             new(5, 17, 2175.13m)
         };
-    public DepositResponse MakeDeposit(DepositRequest request)
+
+    public static BalanceChangeResponse MakeDeposit(BalanceChangeRequest request)
     {
-        if (request == null) throw new ArgumentNullException(nameof(request));
-        if (request.AccountId < 0) throw new ArgumentOutOfRangeException(nameof(request));
-        if (request.CustomerId < 0) throw new ArgumentOutOfRangeException(nameof(request));
-        if (request.Amount < 0) throw new ArgumentException(nameof(request));
-
-        Account accountToAccess = sampleAccounts.SingleOrDefault(account => account.AccountId == request.AccountId && account.CustomerId == request.CustomerId);
-
-        DepositResponse response = new DepositResponse(0,0,0,false);
-
-        if (accountToAccess != null)
+        try
         {
-            accountToAccess.Balance += request.Amount;
-            response.CustomerId = accountToAccess.CustomerId;
-            response.AccountId = accountToAccess.AccountId;
-            response.Balance = accountToAccess.Balance;
-            response.Succeeded = true;
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (request.AccountId < 0) throw new ArgumentOutOfRangeException(nameof(request));
+            if (request.CustomerId < 0) throw new ArgumentOutOfRangeException(nameof(request));
+            if (request.Amount < 0) throw new ArgumentException(nameof(request));
+
+            Account accountToAccess = sampleAccounts.SingleOrDefault(account => account.AccountId == request.AccountId && account.CustomerId == request.CustomerId); //this would be SQL in the real world
+
+            if (accountToAccess == null) throw new Exception();
+            accountToAccess.Deposit(request.Amount);
+            BalanceChangeResponse response = new BalanceChangeResponse(accountToAccess.CustomerId, accountToAccess.AccountId, accountToAccess.Balance, true);
+            return response;
         }
-        return response;
-    }
-
-    public class Account
-    {
-        public int CustomerId { get; set; }
-        public int AccountId { get; set; }
-        public decimal Balance { get; set; }
-
-        public Account(int customerId, int accountId, decimal balance)
+        catch (Exception)
         {
-            CustomerId = customerId;
-            AccountId = accountId;
-            Balance = balance;
+            return new BalanceChangeResponse(0, 0, 0, false);
         }
     }
-    public class DepositRequest
+    public static BalanceChangeResponse MakeWithdrawl(BalanceChangeRequest request)
     {
-        public int CustomerId { get; set; }
-        public int AccountId { get; set; }
-        public decimal Amount { get; set; }
-        public DepositRequest(int customerId, int accountId, decimal amount)
+        try
         {
-            CustomerId = customerId;
-            AccountId = accountId;
-            Amount = amount;
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (request.AccountId < 0) throw new ArgumentOutOfRangeException(nameof(request));
+            if (request.CustomerId < 0) throw new ArgumentOutOfRangeException(nameof(request));
+            if (request.Amount < 0) throw new ArgumentException(nameof(request));
+
+            Account accountToAccess = sampleAccounts.SingleOrDefault(account => account.AccountId == request.AccountId && account.CustomerId == request.CustomerId); //this would be SQL in the real world
+
+            if (accountToAccess == null) throw new Exception();
+            accountToAccess.Withdrawl(request.Amount);
+            BalanceChangeResponse response = new BalanceChangeResponse(accountToAccess.CustomerId, accountToAccess.AccountId, accountToAccess.Balance, true);
+            return response;
+        }
+        catch (Exception)
+        {
+            return new BalanceChangeResponse(0, 0, 0, false);
         }
     }
-    public class DepositResponse
+}
+
+public class Account
+{
+    public int CustomerId { get; set; }
+    public int AccountId { get; set; }
+    public decimal Balance { get; set; }
+
+    public Account(int customerId, int accountId, decimal balance)
     {
-        public int CustomerId { get; set; }
-        public int AccountId { get; set; }
-        public decimal Balance { get; set; }
-        public bool Succeeded { get; set; }
-        public DepositResponse(int customerId, int accountId, decimal balance, bool succeeded)
-        {
-            CustomerId = customerId;
-            AccountId = accountId;
-            Balance = balance;
-            Succeeded = succeeded;
-        }
+        CustomerId = customerId;
+        AccountId = accountId;
+        Balance = balance;
+    }
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+    public void Withdrawl(decimal amount)
+    {
+        Balance -= amount;
+    }
+}
+public class BalanceChangeRequest
+{
+    public int CustomerId { get; set; }
+    public int AccountId { get; set; }
+    public decimal Amount { get; set; }
+    public BalanceChangeRequest(int customerId, int accountId, decimal amount)
+    {
+        CustomerId = customerId;
+        AccountId = accountId;
+        Amount = amount;
+    }
+}
+public class BalanceChangeResponse
+{
+    public int CustomerId { get; set; }
+    public int AccountId { get; set; }
+    public decimal Balance { get; set; }
+    public bool Succeeded { get; set; }
+    public BalanceChangeResponse(int customerId, int accountId, decimal balance, bool succeeded)
+    {
+        CustomerId = customerId;
+        AccountId = accountId;
+        Balance = balance;
+        Succeeded = succeeded;
     }
 }
