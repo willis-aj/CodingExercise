7601aeb988279c0324d30eb4b9f52860b107768b
willis-aj <willis.aj@outlook.com>
2024-05-07 19:10:22 -0500

    added unit tests and close account method

--- CodingExercise/Program.cs
+++ CodingExercise/Program.cs
@@ -25,24 +25,35 @@ myApi.MapPost("/withdrawl", (HttpRequest request) =>
 
     return Results.Json(Bank.MakeWithdrawl(withdrawlRequest), typeof(BalanceChangeResponse), SourceGenerationContext.Default);
 });
+
+myApi.MapPost("/closeAccount", (HttpRequest request) =>
+{
+    var jsonCloseRequest = request.ReadFromJsonAsync<CloseRequest>();
+    CloseRequest closeRequest = new CloseRequest(jsonCloseRequest.Result.CustomerId, jsonCloseRequest.Result.AccountId);
+
+    return Results.Json(Bank.CloseAccount(closeRequest), typeof(CloseResponse), SourceGenerationContext.Default);
+});
 app.Run();
 
-[JsonSerializable(typeof(BalanceChangeResponse))]
 [JsonSerializable(typeof(BalanceChangeRequest))]
+[JsonSerializable(typeof(BalanceChangeResponse))]
+[JsonSerializable(typeof(CloseRequest))]
+[JsonSerializable(typeof(CloseResponse))]
 internal partial class SourceGenerationContext : JsonSerializerContext
 {
 }
 
 
-class Bank
+public class Bank
 {
     //would have liked to make a factory to generate different kinds of accounts (checking, savings, etc.)
     //this is pretend db
     static Account[] sampleAccounts =
         {
-            new(1, 1, 100),
-            new(2, 2, 200),
-            new(5, 17, 2175.13m)
+            new(1, 1, 100, true),
+            new(2, 2, 200, true),
+            new(5, 17, 2175.13m, true),
+            new(7, 7, 0, true)
         };
 
     public static BalanceChangeResponse MakeDeposit(BalanceChangeRequest request)
@@ -87,6 +98,27 @@ class Bank
             return new BalanceChangeResponse(0, 0, 0, false);
         }
     }
+    public static CloseResponse CloseAccount(CloseRequest request)
+    {
+        try
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (request.AccountId < 0) throw new ArgumentOutOfRangeException(nameof(request));
+            if (request.CustomerId < 0) throw new ArgumentOutOfRangeException(nameof(request));
+
+            Account accountToAccess = sampleAccounts.SingleOrDefault(account => account.AccountId == request.AccountId && account.CustomerId == request.CustomerId); //this would be SQL in the real world
+
+            if (accountToAccess == null) throw new Exception();
+            if (accountToAccess.Balance != 0) throw new Exception();
+            accountToAccess.Close(); // this would be SQL in the real world
+            CloseResponse response = new CloseResponse(accountToAccess.CustomerId, accountToAccess.AccountId, true);
+            return response;
+        }
+        catch (Exception)
+        {
+            return new CloseResponse(0, 0, false);
+        }
+    }
 }
 
 public class Account
@@ -94,12 +126,14 @@ public class Account
     public int CustomerId { get; set; }
     public int AccountId { get; set; }
     public decimal Balance { get; set; }
+    public bool Active { get; set; }
 
-    public Account(int customerId, int accountId, decimal balance)
+    public Account(int customerId, int accountId, decimal balance, bool active)
     {
         CustomerId = customerId;
         AccountId = accountId;
         Balance = balance;
+        Active = active;
     }
     public void Deposit(decimal amount)
     {
@@ -108,6 +142,10 @@ public class Account
     public void Withdrawl(decimal amount)
     {
         Balance -= amount;
+    }
+    public void Close()
+    {
+        Active = false;
     }
 }
 public class BalanceChangeRequest
@@ -133,6 +171,28 @@ public class BalanceChangeResponse
         CustomerId = customerId;
         AccountId = accountId;
         Balance = balance;
+        Succeeded = succeeded;
+    }
+}
+public class CloseRequest
+{
+    public int CustomerId { get; set; }
+    public int AccountId { get; set; }
+    public CloseRequest(int customerId, int accountId)
+    {
+        CustomerId = customerId;
+        AccountId = accountId;
+    }
+}
+public class CloseResponse
+{
+    public int CustomerId { get; set; }
+    public int AccountId { get; set; }
+    public bool Succeeded { get; set; }
+    public CloseResponse(int customerId, int accountId, bool succeeded)
+    {
+        CustomerId = customerId;
+        AccountId = accountId;
         Succeeded = succeeded;
     }
 }
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,98 @@
+using Shouldly;
+
+namespace TestProject
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void DepositTest()
+        {
+            // Arrange
+            Account account = new Account(3, 3, 300, true);
+
+            //Act
+            account.Deposit(60);
+
+            //Assert
+            account.Balance.ShouldBe(360);
+        }
+
+        [TestMethod]
+        public void WithdrawlTest()
+        {
+            // Arrange
+            Account account = new Account(3, 3, 300, true);
+
+            //Act
+            account.Withdrawl(60);
+
+            //Assert
+            account.Balance.ShouldBe(240);
+        }
+
+        [TestMethod]
+        public void CloseTest()
+        {
+            // Arrange
+            Account account = new Account(3, 3, 300, true);
+
+            //Act
+            account.Close();
+
+            //Assert
+            account.Active.ShouldBe(false);
+        }
+    }
+
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void DepositTest()
+        {
+            // Arrange
+            BalanceChangeRequest request = new(1,1,50);
+
+            //Act
+            BalanceChangeResponse response = Bank.MakeDeposit(request);
+
+            //Assert
+            response.Balance.ShouldBe(150);
+            response.AccountId.ShouldBe(1);
+            response.CustomerId.ShouldBe(1);
+            response.Succeeded.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void WithdrawlTest()
+        {
+            // Arrange
+            BalanceChangeRequest request = new(2, 2, 50);
+
+            //Act
+            BalanceChangeResponse response = Bank.MakeWithdrawl(request);
+
+            //Assert
+            response.Balance.ShouldBe(150);
+            response.AccountId.ShouldBe(2);
+            response.CustomerId.ShouldBe(2);
+            response.Succeeded.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void CloseTest()
+        {
+            // Arrange
+            CloseRequest request = new(7, 7);
+
+            //Act
+            CloseResponse response = Bank.CloseAccount(request);
+
+            //Assert
+            response.AccountId.ShouldBe(7);
+            response.CustomerId.ShouldBe(7);
+            response.Succeeded.ShouldBeTrue();
+        }
+    }
+}
